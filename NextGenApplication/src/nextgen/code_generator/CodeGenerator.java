f9ac6fa8056872894aaac6d7377385c98f60f92f
Rodrigo Diaz <pericodiaz89@gmail.com>
2013-06-11 12:46:19 -0500

    Added base classes for the packages

---
+++ NextGenApplication/src/nextgen/code_generator/CodeGenerator.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextgen.code_generator;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class CodeGenerator {
+}
---
+++ NextGenApplication/src/nextgen/dao/FileManager.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextgen.dao;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class FileManager {
+}
