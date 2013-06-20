82d563c9b94bf696b62670cf36ebd72a67b3a22e
Santiago Moreno <smoreno91@gmail.com>
2013-06-20 11:42:25 -0500

    getProjects almost complete

--- NextGenApplication/src/nextgen/dao/DAO.java
+++ NextGenApplication/src/nextgen/dao/DAO.java
@@ -6,8 +6,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import lib.json.JSONException;
 import lib.json.JSONObject;
+import nextgen.model.Attribute;
 import nextgen.model.Element;
+import nextgen.model.Entity;
 import nextgen.model.Project;
+import nextgen.model.Package;
+import nextgen.model.enums.Cardinality;
 
 /**
  *
@@ -21,15 +25,18 @@ public class DAO {
         fileManager = new FileManager();
     }
 
-    public Project getProjects(String dir) throws JSONException{
-        JSONObject obj = new JSONObject();        
+    public Project getProjects(String dir) throws JSONException{        
         try {
+            JSONObject obj;
             obj = fileManager.loadData(dir);
             
+            String nameProject = obj.getString("name");
+            String descriptionProject = obj.getString("description");
+            HashSet<Element> elementList = new HashSet<>();
+            
             JSONObject elements = obj.getJSONObject("elements");
-                        
             Iterator i = elements.keys();
-            while(i.hasNext()){  
+            while(i.hasNext()){
                 JSONObject elem = (JSONObject) i.next();
                 JSONObject keys = elem.getJSONObject("keys");
                 if(keys != null){
@@ -43,38 +50,48 @@ public class DAO {
                 String descriptionPackage = packageElement.getString("description");
                 String namePackage = packageElement.getString("name");
                 
+                Package packageModel = new Package(namePackage, descriptionPackage);
+                
                 JSONObject attributes = elem.getJSONObject("attributes");
-                if (attributes != null){
-                    
+                HashSet<Attribute> attributeList = new HashSet<>();
+                
+                if (attributes != null){                    
                     Iterator attributeIter = attributes.keys();
                     while(attributeIter.hasNext()){
                         JSONObject attr = (JSONObject) attributeIter.next();
-                        String autoIncrement = attr.getString("true");
+                        String autoIncrement = attr.getString("autoincrement");
                         String nameAttr = attr.getString("name");
                         
                         JSONObject entity = attr.getJSONObject("entity");
                         String descriptionEntity = entity.getString("description");
                         String nameEntity = entity.getString("name");
                         
+                        Entity entityModel = new Entity(nameEntity, descriptionEntity);
+                        
                         String commonTable = entity.getString("commonTable");
                         String required = entity.getString("required");
                         String comment = entity.getString("comment");
                         String defaultValue = entity.getString("defaultValue");
                         String cardinality = entity.getJSONObject("cardinality").toString();
+                        
+                        Cardinality c;
+                        if(cardinality.equals("Multiple")){
+                            c = Cardinality.Multiple;
+                        }else{
+                            c = Cardinality.Single;
+                        }              
+                        attributeList.add(new Attribute(1, nameAttr, entityModel, c, Boolean.parseBoolean(required), comment, defaultValue, Boolean.parseBoolean(autoIncrement), commonTable));                        
                     }
                 }
+                
+                elementList.add(new Element(nameElement, descriptionElement, tableNameElement, packageModel, null, attributeList, null));
             }
+           return new Project(nameProject, descriptionProject, elementList);
         } catch (Exception ex) {
             ex.getMessage();
         }        
         return null;
     }
-
-     public HashSet<Element> getElements(JSONObject obj){
-         HashSet<Element> elementList = new HashSet<>();
-         
-         return null;
-     }
     
     public void saveProject(Project project, String path) throws Exception {
         JSONObject obj = new JSONObject(project.toHashMap());
