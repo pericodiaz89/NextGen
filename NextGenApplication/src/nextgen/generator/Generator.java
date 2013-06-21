3b9c8d7025225abd0ab3a4f957abe90d151b0b83
Rodrigo Diaz <pericodiaz89@gmail.com>
2013-06-20 21:34:42 -0500

    Initial commit for generation

---
+++ NextGenApplication/src/nextgen/generator/Generator.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextgen.generator;
+
+public abstract class Generator {
+
+    public abstract void generate(String directory) throws Exception;
+}
